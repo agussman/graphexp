28674510afb40a8cc78c71a4b39d5a6c0ed6f020
Aaron Gussman <agussman@gmail.com>
2018-10-09 22:42:19 -0400

    Working through the code

--- scripts/graph_viz.js
+++ scripts/graph_viz.js
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Main module. Handle the viualization, display data and layers.
+// Main module. Handle the visualization, display data and layers.
 
 var graph_viz = (function(){
 	"use strict";
